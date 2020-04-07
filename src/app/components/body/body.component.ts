cb0a8ebc1bbf135f115d6f347e2bc313d3994098
mira-ceti <ibellon@gmail.com>
2020-04-07 21:58:01 +0200

    Final del Proyecto

--- src/app/components/body/body.component.ts
+++ src/app/components/body/body.component.ts
@@ -4,4 +4,14 @@ import {Component} from '@angular/core';
     selector: 'app-body',
     templateUrl: './body.component.html'
 })
-export class BodyComponent {}
+export class BodyComponent {
+
+    mostrar = true;
+
+    frase: any = {
+        mensaje: 'Una gran poder requiere una gran responsabilidad',
+        autor: 'Ben Parker'
+    };
+
+    personajes: string[] = ['Spiderman', 'Superman', 'Dr. Octpus', 'Lex Luthor'];
+}
